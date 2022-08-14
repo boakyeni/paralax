1038c3da9a1461e09ed7b82c0d8da5c8886097fe
Kojo Nimako <boakyeni@usc.edu>
2022-08-13 21:19:10 -0700

    added reviews

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import Carousel from './carousel'
 import CarouselItem from './carousel-item'
+import Review from './review'
 
 const Testimonials: React.FC = () => (
     <Carousel className='bg-black text-white py-10 lg:py-20'>
         <CarouselItem index={0}>
-            <div>Such a good book club</div>
+            <Review by="my mans">
+                Such a good book club
+            </Review>
+        </CarouselItem>
+        <CarouselItem index={1}>
+            <Review by="other dude">
+                Really so good
+            </Review>
+        </CarouselItem>
+        <CarouselItem index={2}>
+            <Review by="this guy">
+                Join this bookclub
+            </Review>
+        </CarouselItem>
+        <CarouselItem index={3}>
+            <Review by="test">
+                it's a good bookclub
+            </Review>
         </CarouselItem>
     </Carousel>
 )
